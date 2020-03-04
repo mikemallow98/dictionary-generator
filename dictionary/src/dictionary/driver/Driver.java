6cad7f9d352f1a1234592f8a8f3a1a79ae27de71
mike <herewegolakers@gmail.com>
2020-03-04 18:35:24 -0500

    Added driver code to test the document handler and file parser

--- dictionary/src/dictionary/driver/Driver.java
+++ dictionary/src/dictionary/driver/Driver.java
@@ -1,10 +1,31 @@
 package dictionary.driver;
 
+import dictionary.document.DocumentI;
+import dictionary.document.LineBasedDocumentHandler;
 import dictionary.tokenizer.Tokenizer;
+import dictionary.util.Term;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
 
 public class Driver{
 	public static void main(String[] args){
-		System.out.println("Hello world");
+		DocumentI docHandler;
+		List<Term> terms = new ArrayList<Term>();
+		try{
+			docHandler = new LineBasedDocumentHandler("test.txt");
+			terms = (List<Term>) docHandler.parseTerms();
+			for(int i = 0; i < terms.size(); i++){
+				Term tmp = (Term) terms.get(i);
+				System.out.println(tmp.getTerm() + "\t" + tmp.getDocID());
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
 
